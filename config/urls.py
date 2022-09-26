8c73100e27464971518e1e41886a616a778e32d9
Jimmy <jimmy.a.smith@gmail.com>
2022-09-26 15:45:57 -0700

    updated config.urls

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import path,include
-
+from django.urls import path
+from django.conf.urls import include
+from posts.views import AboutPageView, HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
-    path('', include('pages.urls')),
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
